f5aef93379c4168270608d10c916af17f85137d7
David <davidongaroh@gmail.com>
2022-09-27 15:49:23 +0300

    solution

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,19 +7,27 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
+    toy = Toy.create(toy_params)
     render json: toy, status: :created
   end
 
   def update
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.update(toy_params)
+    render json: toy, status: :ok
   end
 
   def destroy
-    toy = Toy.find_by(id: params[:id])
+    toy = find_toy
     toy.destroy
     head :no_content
+  end
+
+  def add_likes
+    toy = find_toy
+    toy.likes += 1
+    toy.update(toy_params)
+  render json: toy
   end
 
   private
@@ -27,5 +35,9 @@ class ToysController < ApplicationController
   def toy_params
     params.permit(:name, :image, :likes)
   end
+  def find_toy
+    toy = Toy.find(params[:id])
+  end
 
+  
 end
